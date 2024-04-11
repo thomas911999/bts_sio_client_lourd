0f838469fc29f7da5c1ef195fa223aac6ea47302
thomas <thomas911999@hotmail.fr>
2024-04-11 15:50:19 +0200

    first page voyageexpress + Ligne de commande FX

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+Run java Project : .\gradlew run
+
+
 # bts_sio_client_lourd
---
+++ voyage/app/src/main/java/com/efrei/MainApp.java
@@ -0,0 +1,30 @@
+package com.efrei;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
+        
+        // Add the ImageView to your scene or layout
+        // For example, if you want to add it to the root node:
+
+        Scene scene = new Scene(root);
+        
+        stage.setTitle("Administration VoyageExpress");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
